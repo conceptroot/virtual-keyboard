8f7090bdef50d8d43f9f91bb6f04be47937739ad
Alisher Beknazarov <conceptroot@gmail.com>
2023-04-27 11:06:49 +0530

    feat: add custom event when virtual keybord press

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import { Keyboard } from "./js/keyboard"
 
 window.onload = async () => {
     const keyboard = new Keyboard()
+    
+    document.querySelector('body').addEventListener('virtual_kb_press', e => {
+        console.log('triggerd virtual_kb_press', e.detail)
+    }, false)
 }
--- src/js/key.js
+++ src/js/key.js
@@ -1,3 +1,4 @@
+
 export class Key {
     constructor (key_config) {
         this.id = key_config.id
@@ -13,10 +14,25 @@ export class Key {
     }
     initEventlistners() {
         this.html.addEventListener('click', (e) => {
+            // Запуск кастомного ивента
+            const virtual_kb_press_event = new CustomEvent(
+                "virtual_kb_press", 
+                {
+                    bubbles: true,
+                    detail: {
+                        id: this.id,
+                        key: this.layers['en'] //TODO
+                    }
+                })
+            this.html.dispatchEvent(virtual_kb_press_event)
+
+            // Визуализация и анимация кнопки
             this.html.classList.toggle('key_press')
             setTimeout(e => {
                 this.html.classList.toggle('key_press')
             }, 600)
+
+
         })
     }
     createElement() {
@@ -28,7 +44,7 @@ export class Key {
         } else if (this.style === 'secondary') {
             key.classList.add('key_secondary')
         }
-        key.textContent = this.layers['en']
+        key.textContent = this.layers['en'] // TODO
         this.html = key
         console.log('#key=', this.html)
         // удалить TODO
